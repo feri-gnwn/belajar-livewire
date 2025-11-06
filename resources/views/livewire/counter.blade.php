1df75301952f9b76e9e66013abbb5b45d0f05d02
feri-gnwn <ferigunawan1505@gmail.com>
2025-11-06 09:51:16 +0700

    ye counter

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,7 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
+    <h1>{{ $count }}</h1>
+ 
+    <button wire:click="increment">+</button>
+ 
+    <button wire:click="decrement">-</button>
 </div>
